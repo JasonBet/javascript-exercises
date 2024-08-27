6d94b2dec0319cca10881e9b449ceb40da667e69
Jason <jasonbetsargon@gmail.com>
2024-08-26 18:26:22 -0700

    Pass tests for temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cTemp = Math.round(((temp - 32) * (5 / 9)) * 10) / 10;
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fTemp = Math.round((temp * (9/5) + 32) * 10) / 10;
+  return fTemp;
 };
 
 // Do not edit below this line
