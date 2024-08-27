d2f9798ec055fc1bb282a111ed47e0f8dd98b253
Jason <jasonbetsargon@gmail.com>
2024-08-26 18:09:00 -0700

    Pass tests for sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    let small;
+    let big;
+    let sumAll = 0;
+    if (a < b) {small = a; big = b;}
+    else {small = b; big = a;}
+    for (let i = small; i <= big; i++) {
+        sumAll += i;
+    }
 
+    return sumAll;
 };
 
 // Do not edit below this line
