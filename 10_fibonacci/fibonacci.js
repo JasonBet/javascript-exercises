b16de5f1fa8ec272f798383a7995f10910f07c2b
Jason <jasonbetsargon@gmail.com>
2024-09-03 06:49:49 -0700

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let number = Number(num);
+    if(number === 0) return 0;
+    if(number < 0) return "OOPS";
+    let prev = 0;
+    let temp = 0;
+    let curr = 1;
+    for(let i=0; i<number; i++){
+        temp = curr;
+        curr += prev; 
+        prev = temp;             
+    }
+    return prev;
 };
 
 // Do not edit below this line
