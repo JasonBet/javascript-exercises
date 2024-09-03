343dbe8ef8c769cf73ebf0fde42854bfb52d7d0f
Jason <jasonbetsargon@gmail.com>
2024-09-02 18:25:24 -0700

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let arr = string.split('');
+    arr = arr.filter(char => char !== '.' && char !== ',' && char !== ' ' && char !='!');
+    for(let i=0; i<=arr.length / 2; i++){
+        if(arr[i] !== arr[arr.length - 1 - i]) {return false;}
+    }
+    return true;
 };
 
 // Do not edit below this line
